5ca46bf5c009148592bb012a635e621d5e3136e2
lachuginTech <vladimir.lachugin.tech@gmail.com>
2024-10-16 13:11:10 +0300

    0.2
    displaying a hint when the mouse cursor hovers over an element

--- src/extension.ts
+++ src/extension.ts
@@ -1,47 +1,31 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-
-    
-    let disposable = vscode.workspace.onDidChangeTextDocument((event) => {
-        const editor = vscode.window.activeTextEditor;
-
-        if (editor && editor.document.languageId === 'css') {
-            const document = editor.document;
-            const text = document.getText();
-            const regex = /(\d+)px/g;
-            let match;
-
-       
-            while ((match = regex.exec(text)) !== null) {
-                const pxValue = parseFloat(match[1]);
-
-               
-                const line = document.lineAt(document.positionAt(match.index).line);
-                if (line.text.includes('/*')) {
-                    continue;
-                }
+  
+    let hoverProvider = vscode.languages.registerHoverProvider('css', {
+        provideHover(document, position, token) {
+           
+            const range = document.getWordRangeAtPosition(position, /(\d+)px/);
+            if (range) {
+                const word = document.getText(range);
+                const pxValue = parseFloat(word);
 
                
                 const remValue = (pxValue / 16).toFixed(2);
                 const emValue = (pxValue / 16).toFixed(2);
                 const vwValue = ((pxValue / document.lineAt(0).range.end.character) * 100).toFixed(2);
 
-                const comment = ` /* ${pxValue}px = ${remValue}rem = ${emValue}em = ${vwValue}vw */`;
+                
+                const hoverText = `${pxValue}px = ${remValue}rem = ${emValue}em = ${vwValue}vw`;
 
               
-                const edit = new vscode.WorkspaceEdit();
-                const position = document.positionAt(match.index + match[0].length);
-                edit.insert(document.uri, position, comment);
-
-           
-                vscode.workspace.applyEdit(edit);
+                return new vscode.Hover(hoverText);
             }
         }
     });
 
-    context.subscriptions.push(disposable);
+   
+    context.subscriptions.push(hoverProvider);
 }
-
 
 export function deactivate() {}
